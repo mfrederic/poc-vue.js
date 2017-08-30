f7c03e57b675d72e7eb5a04c2a2c0b6d03737052
Frederic Maury <fmaury.ext@almerys.com>
2017-08-30 15:47:36 +0200

    Initalize the project with vue-cli and webpack

---
+++ src/stores/modules/item..js
@@ -0,0 +1,27 @@
+/* import Vue from 'Vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const itemStore = new Vuex.Store({
+  state: {
+    items: [],
+    defaultItem: {
+      id: null,
+      label: '',
+      description: '',
+      date: new Date(),
+      vehiculeState: null,
+      cost: 0,
+      keywords: [],
+      location: '',
+      paymentType: ''
+    },
+    count: 0
+  },
+  getters: {
+    log () {
+      console.log(this)
+    }
+  }
+}) */
